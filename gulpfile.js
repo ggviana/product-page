4ebd7f7a5e441e56e4d9719e116a2342ae9a82cf
Guilherme Viana <ggviana@hotmail.com.br>
2016-10-23 22:47:50 -0200

    fixing tasks and setting stylus as preprocessor

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const concat      = require('gulp-concat')
 const htmlmin     = require('gulp-htmlmin')
 const postcss     = require('gulp-postcss')
 const uglify      = require('gulp-uglify')
-const sass        = require('gulp-sass')
+const stylus      = require('gulp-stylus')
 const browserSync = require('browser-sync')
 const runSequence = require('run-sequence')
 const reload      = browserSync.reload
@@ -19,33 +19,50 @@ gulp.task('default', ['build'], () => {
       server: 'dist/'
     })
 
-  gulp.watch('src/javascript/*.js', ['javascript']).on('change', reload)
-  gulp.watch('src/sass/*.scss', ['css'])
-  gulp.watch('src/*.html', ['html']).on('change', reload)
-    
+  gulp
+    .watch('src/javascript/*.js', ['javascript'])
+    .on('change', reload)
+
+  gulp
+    .watch('src/stylus/*.styl', ['css'])
+    .on('change', reload)
+
+  gulp
+    .watch('src/*.html', ['html'])
+    .on('change', reload)
 })
 
 gulp.task('build', () => 
-  runSequence(['css', 'javascript', 'html']))
+  runSequence(['css', 'javascript', 'html', 'copy-assets']))
+
+gulp.task('copy-assets', () => gulp
+  .src('node_modules/chico/dist/assets/*')
+  .pipe(gulp.dest('dist/assets')))
 
 gulp.task('html', () => gulp
   .src('src/*.html')
-  .pipe(htmlmin())
+  .pipe(htmlmin({ collapseWhitespace: true }))
   .pipe(gulp.dest('dist/')))
 
 gulp.task('javascript', () => gulp
-  .src('src/javascript/*.js')
+  .src([
+    'node_modules/chico/dist/ui/chico.js',
+    'src/javascript/*.js'
+  ])
   .pipe(concat('app.js'))
   .pipe(uglify())
   .pipe(gulp.dest('dist/')))
 
 gulp.task('css', () => gulp
-  .src('src/sass/_index.scss')
-  .pipe(sass())
+  .src('src/stylus/_index.styl')
+  .pipe(stylus({
+    paths: ['node_modules'],
+    'include css': true
+  }))
+  .pipe(concat('styles.css'))
   .pipe(postcss([
     autoprefixer({ browsers: ['last 1 version'] }),
     cssnano()
   ]))
-  .pipe(gulp.dest('dist/'))
-  .pipe(browserSync.stream()))
+  .pipe(gulp.dest('dist/')))
 
